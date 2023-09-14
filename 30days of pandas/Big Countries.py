56d6514754bc46f9cc17fb54ca8e6f6322a749f8
aybüke <77280855+cescedes@users.noreply.github.com>
2023-09-14 20:13:43 +0200

    Update Big Countries.py

--- 30days of pandas/Big Countries.py
+++ 30days of pandas/Big Countries.py
@@ -17,3 +17,9 @@ import pandas as pd
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     big_country = world[(world['area']>=3000000)|(world['population']>=25000000)]
     return big_country[['name','population','area']]
+
+'''
+import pandas as pd
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:  
+    return world[(world.population >= 25000000) | (world.area  >= 3000000)].filter(items=['name', 'population', 'area'])
+'''
