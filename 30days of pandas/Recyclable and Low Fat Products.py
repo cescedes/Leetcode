fced6bdcaa6f2c644306e3cd94105f5916db22c9
aybüke <77280855+cescedes@users.noreply.github.com>
2023-09-14 20:08:29 +0200

    Create Recyclable and Low Fat Products.py

---
+++ 30days of pandas/Recyclable and Low Fat Products.py
@@ -0,0 +1,8 @@
+'''Write a solution to find the ids of products that are both low fat and recyclable.
+Return the result table in any order.
+The result format is in the following example.'''
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    result = products[(products['low_fats']=='Y')&(products['recyclable']=='Y')]
+    return result[['product_id']]
