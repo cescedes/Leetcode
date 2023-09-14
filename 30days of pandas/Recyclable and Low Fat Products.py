8a2994fe8696bb53f12940c2a535e99af43f9ad7
aybüke <77280855+cescedes@users.noreply.github.com>
2023-09-14 20:11:46 +0200

    Update Recyclable and Low Fat Products.py

--- 30days of pandas/Recyclable and Low Fat Products.py
+++ 30days of pandas/Recyclable and Low Fat Products.py
@@ -6,3 +6,12 @@ import pandas as pd
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     result = products[(products['low_fats']=='Y')&(products['recyclable']=='Y')]
     return result[['product_id']]
+
+'''
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products.loc[(products['low_fats'] == 'Y') &
+    (products['recyclable'] == 'Y'), 
+    ['product_id']]    
+'''
