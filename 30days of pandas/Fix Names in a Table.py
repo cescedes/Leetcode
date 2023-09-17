f8211b36494ee498183daae396862d1ace1232af
aybüke <77280855+cescedes@users.noreply.github.com>
2023-09-17 22:18:33 +0200

    Create Fix Names in a Table.py

---
+++ 30days of pandas/Fix Names in a Table.py
@@ -0,0 +1,11 @@
+'''
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+'''
+
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users['name']=users['name'].str.capitalize()
+    return users.sort_values(by='user_id')
